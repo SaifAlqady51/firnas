da775d8e7c86d5e0c52d78351295ca89df3729e7
SaifAlqady51 <saifalqady51@gmail.com>
2023-12-09 12:33:47 +0200

    changing styles

---
+++ src/api/register.ts
@@ -0,0 +1,29 @@
+
+import axios, { AxiosError } from "axios";
+import { LoginResponse } from "../types/loginResponse-type";
+
+type AxiosErrorData = {
+    statusCode: number;
+    message: string;
+};
+
+export const userRegister = async (name:string | undefined,email: string, password: string) => {
+    try {
+        const user = await axios.post(
+            "https://airport-api-production-9cfb.up.railway.app/users/register",
+            {name, email, password },
+        );
+        // const user = await axios(config)
+        const status = "success";
+        const message = "logged in successfully";
+        const res: LoginResponse = { message, status };
+        return res;
+    } catch (e) {
+        const error = e as AxiosError;
+        const dataError = error?.response?.data as AxiosErrorData;
+        const status = "error";
+        const message = dataError.message;
+        const res: LoginResponse = { message, status };
+        return res;
+    }
+};
