6be2d049bf10aa4dfc995d4d79e27c3f5845f36f
SaifAlqady51 <saifalqady51@gmail.com>
2024-01-05 10:40:40 +0200

    made firnas logo clickable

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,16 +1,19 @@
 import { SideNavbar } from "./sideNavbar";
 import { useState } from "react";
 import { TopNavbar } from "./topNavbar";
+import Link from "next/link";
 
 const NavBar = () => {
     const [user, setUser] = useState({});
 
     return (
         <div
-            className={`text-textColor fixed  inset-0 z-50 mx-auto mt-4 flex h-24 w-11/12 items-center justify-center rounded-full bg-nav font-bold backdrop-blur-sm dark:text-[#fefae0] lg:justify-around`}
+            className={`fixed inset-0  z-50 mx-auto mt-4 flex h-24 w-11/12 items-center justify-center rounded-full bg-nav font-bold text-textColor backdrop-blur-sm dark:text-[#fefae0] lg:justify-around`}
         >
             {/* website Name */}
-            <div className="text-4xl font-bold">Firnas</div>
+            <Link className="text-4xl font-bold" href="/">
+                Firnas
+            </Link>
 
             {/* side navbar appears only in md screen size*/}
             <SideNavbar user={user} setUser={setUser} />
