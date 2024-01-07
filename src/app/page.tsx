fb127e49f07bb98afaef620c7537bd733ee63294
SaifAlqady51 <saifalqady51@gmail.com>
2024-01-07 15:22:11 +0200

    move main div to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,20 @@ import { Endpoint } from "@/components/home/endpoint";
 
 export default function Home() {
     return (
-        <>
-            <Endpoint
-                inputList={["Pass", "Airport IATA code", "EX: CAI"]}
-                outputList={["airportName", "cityName", "countryName"]}
-                link="/airportInfo"
-            />
-            <Endpoint
-                inputList={["Pass", "City name", "EX: Cairo"]}
-                outputList={["airportName", "airportCode"]}
-                outputType="list"
-                link="/"
-            />
-        </>
+        <main className=" align-center dark z-0 flex h-full flex-col overscroll-x-none bg-[#e2eafc]  dark:bg-dark  dark:text-textColor  ">
+            <div className="mx-40 my-40 flex h-fit flex-col items-center justify-center">
+                <Endpoint
+                    inputList={["Pass", "Airport IATA code", "EX: CAI"]}
+                    outputList={["airportName", "cityName", "countryName"]}
+                    link="/airportInfo"
+                />
+                <Endpoint
+                    inputList={["Pass", "City name", "EX: Cairo"]}
+                    outputList={["airportName", "airportCode"]}
+                    outputType="list"
+                    link="/"
+                />
+            </div>
+        </main>
     );
 }
