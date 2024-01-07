512c7360c80a0644e5c1743b4a2a2084f600d2e6
SaifAlqady51 <saifalqady51@gmail.com>
2024-01-07 15:21:56 +0200

    created ariprotInfo Page

--- src/app/airportInfo/page.tsx
+++ src/app/airportInfo/page.tsx
@@ -1,5 +1,48 @@
+"use client";
+import { useState } from "react";
+import { IoCopy } from "react-icons/io5";
 const AirportInfoPage = () => {
-    return <h1> Airport info</h1>;
+    const [showCopiedMessage, setShowCopiedMessage] = useState(false);
+    const copyText = () => {
+        navigator.clipboard.writeText(
+            "https://airport-api-gamma.vercel.app/api/airport/get/airportName?airportCode=",
+        );
+        setShowCopiedMessage(true);
+        setTimeout(() => {
+            setShowCopiedMessage(false);
+        }, 1000);
+    };
+    return (
+        <main className=" align-center dark z-0 flex h-full flex-col items-center overscroll-x-none bg-[#e2eafc]  dark:bg-dark  dark:text-textColor  ">
+            <div className="my-60   flex h-fit w-4/5 flex-col items-center justify-center">
+                <div className="mb-12 text-xl font-semibold">
+                    Use this endpoint if you want to to get airport information
+                    from airport IATA code{"  "}
+                    <p className="mt-6">
+                        EX.{" "}
+                        https://airport-api-gamma.vercel.app/api/airport/get/airportName?airportCode=CAI{" "}
+                    </p>
+                </div>
+                <div
+                    onClick={copyText}
+                    className=" relative flex w-11/12 items-center justify-around rounded-lg border-2 border-black bg-black   px-2 py-4 text-black dark:bg-blue-50 lg:w-full xl:w-10/12  2xl:w-7/12"
+                >
+                    <p>
+                        https://airport-api-gamma.vercel.app/api/airport/get/airportName?airportCode=&#123;IATACode&#125;{" "}
+                    </p>
+                    <div className=" flex cursor-pointer items-center justify-center">
+                        <IoCopy className="text-xl" />
+                    </div>
+                    {showCopiedMessage && (
+                        <div className=" absolute right-4 top-12 h-6 w-16 rounded-lg bg-slate-500 text-center text-white ">
+                            {" "}
+                            copied
+                        </div>
+                    )}
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default AirportInfoPage;
