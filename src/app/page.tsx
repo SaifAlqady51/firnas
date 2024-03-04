951c4f6dd3c3ce2e3fc5e1f78ca1041ba7969582
SaifAlqady51 <saifalqady51@gmail.com>
2024-03-04 19:46:28 +0200

    style: made main page height 100vh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Endpoint } from "@/components/home/endpoint";
 
 export default function Home() {
     return (
-        <main className=" align-center dark z-0 flex h-full flex-col overscroll-x-none bg-[#e2eafc]  dark:bg-dark  dark:text-textColor  ">
+        <main className=" align-center dark z-0 flex h-screen flex-col overscroll-x-none bg-[#e2eafc]  dark:bg-dark  dark:text-textColor  ">
             <div className="mx-40 my-40 flex h-fit flex-col items-center justify-center">
                 <Endpoint
                     inputList={["Pass", "Airport IATA code", "EX: CAI"]}
